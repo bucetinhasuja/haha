034a87681674cbe5790305e3a76148ac6aa22a3e
"João" <joaoscherer.ss070@academico.ifsul.edu.br>
2023-02-23 08:23:31 -0300

    Adicionado funcionalidade digitar o nome e imprimir na tela

--- TRABALHO_GIT_2K/src/Main.java
+++ TRABALHO_GIT_2K/src/Main.java
@@ -1,9 +1,12 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner t = new Scanner(System.in);
+		System.out.println("digite seu nome: ");
+		String a = t.nextLine();
+		System.out.println("olá" + a);
 		System.out.println("olá mundo");
-
 	}
 
 }
